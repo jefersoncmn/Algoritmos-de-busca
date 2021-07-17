896ba1b1ec02ce56c5ad72e1297c1d60e5a92019
jefersoncmn <jefersoncmn@hotmail.com>
2021-07-17 19:49:21 -0300

    feat: Algoritmo de busca gulosa feito

--- Assets/Scripts/PathFindingState.cs
+++ Assets/Scripts/PathFindingState.cs
@@ -13,19 +13,22 @@ public class PathFindingState : SimulatorState
     /// 
     /// </summary>
     /// <param name="cellmapObject"></param>
-    public PathFindingState(GameObject[] cellmapObject)
+    public PathFindingState(GeneralController generalController)
     {
         Debug.Log("Estado PathFinding");
-        for (int i = 0; i < cellmapObject.Length; i++)
+        for (int i = 0; i < generalController.cellmap.Length; i++)
         {
-            cellmap[i] = cellmapObject[i].GetComponent<Cell>() as Cell;
+            cellmap[i] = generalController.cellmap[i].GetComponent<Cell>() as Cell;
             if (i == 35)
             {
                 cellmap[i].endPoint = true;
             }
         }
-        //BuscaLargura(cellmap);
-        //BuscaProfundidade(cellmap);
+
+        //generalController.sucessorFuctionLargura = BuscaLargura(cellmap);
+        //generalController.sucessorFuctionProfundidade = BuscaProfundidade(cellmap);
+        BuscaGulosa(cellmap);
+        //Debug.Log("Distancia = " + Vector3.Distance(cellmap[0].gameObject.transform.position, cellmap[35].gameObject.transform.position));
     }
 
     /// <summary>
@@ -181,8 +184,93 @@ public class PathFindingState : SimulatorState
 
     Cell[] BuscaGulosa(Cell[] cells)
     {
+        Debug.Log("Busca gulosa inicializada!");
+        List<Cell> verticesMarcados = new List<Cell>();
+        List<Cell> melhoresValoresHeuristicos = new List<Cell>();
+
+        Cell ponteiro;
+
+        ponteiro = cells[0];
+        verticesMarcados.Add(ponteiro);
+
+        BuscaGulosaAlgoritmo(ponteiro, verticesMarcados, melhoresValoresHeuristicos);
+
+        for (int i = 0; i < verticesMarcados.Count; i++)
+        {
+            Debug.Log("Posição percorrida " + i + " celula =" + verticesMarcados[i].coins);
+        }
+
         return cells;
     }
+
+
+    List<Cell> BuscaGulosaAlgoritmo(Cell cell, List<Cell> verticesMarcados, List<Cell> melhoresValoresHeuristicos)
+    {
+        Cell[] adjacente = new Cell[4];
+
+        if (cell.right != cell)
+        { //Caso ele aponte para o anterior ele não vai pra trás novamente
+            adjacente[0] = cell.right;//meio que aqui era pra receber a lista de adjacencia
+        }
+        if (cell.left != cell)
+        {
+            adjacente[1] = cell.left;
+        }
+        if (cell.up != cell)
+        {
+            adjacente[2] = cell.up;
+        }
+        if (cell.down != cell)
+        {
+            adjacente[3] = cell.down;
+        }
+        //Debug.Log("Ponteiro =" + cell.coins);
+        if (cell.endPoint == true)
+        { //verifica se o cara é o objetivo
+            //Debug.Log("Objetivo encontrado!" + "Celula " + cell.coins);
+            return null;
+        }
+
+        for (int i = 0; i < 4; i++)//ele percorre todos as arestas
+        {
+
+            if (adjacente[i] != null)
+            {
+
+                if (!verticesMarcados.Contains(adjacente[i]) && !cell.endPoint == true)
+                {
+                    verticesMarcados.Add(adjacente[i]);
+                    //Debug.Log("Celula percorrida " + adjacente[i].coins);
+                    melhoresValoresHeuristicos.Add(adjacente[i]);
+                }
+            }
+        }
+        Cell melhorCelula = melhoresValoresHeuristicos[0];
+        for (int i = 0; i < melhoresValoresHeuristicos.Count; i++) //isso vai assegurar que ele só vai andar pelo que tem a melhor heurística
+        {
+            if (Vector3.Distance(melhoresValoresHeuristicos[i].gameObject.transform.position, cellmap[34].gameObject.transform.position) < Vector3.Distance(melhorCelula.gameObject.transform.position, cellmap[34].gameObject.transform.position))
+            {
+                //Debug.Log("Distancia da celula = " + melhorCelula + " ate o objetivo é = " + Vector3.Distance(melhorCelula.gameObject.transform.position, cellmap[34].gameObject.transform.position));
+                melhorCelula = melhoresValoresHeuristicos[i];
+                if (Vector3.Distance(melhorCelula.gameObject.transform.position, cellmap[34].gameObject.transform.position) == 0)
+                {
+                    Debug.Log("Objetivo encontrado!" + "Celula " + cell.coins);
+                    return null;
+                }
+                //Debug.Log("Distancia da celula atual = " + melhorCelula + " ate o objetivo é = " + Vector3.Distance(melhorCelula.gameObject.transform.position, cellmap[34].gameObject.transform.position));
+            }
+
+        }
+
+        if (melhorCelula != null)
+        {
+            BuscaGulosaAlgoritmo(melhorCelula, verticesMarcados, melhoresValoresHeuristicos);
+        }
+
+
+        return null;
+    }
+
 
     Cell[] BuscaAStar(Cell[] cells)
     {
